85c5c1f3ea46c55fc41a7a885324d17f8c78ab00
ShubhamRao11 <72387919+ShubhamRao11@users.noreply.github.com>
2020-11-28 16:44:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+var a;
+var circles=[];
+function setup() {
+  createCanvas(800,800);
+  stroke(255)
+block1 = new block (200,235,30,40);
+block2 = new block (220,235,30,40);
+block3 = new block (240,235,30,40);
+block4 = new block (250,235,30,40);
+
+
+
+
+
+ camera=new Camera(width/2,height/2,0.5);
+  camera.on();
+  a=height;
+  circles.push(width/2)
+}
+
+function draw() {
+  camera.zoom=camera.zoom+1
+  background("brown");  
+  
+  
+  a=a-1;
+  camera.zoom=camera.zoom+0.01
+ camera.position={x:width/2,y:a}
+ 
+  
+  for (i=0;i<circles.length;i++)
+{
+	circle(circles[i], height/2,20)
+}
+if(camera.position.x%width===0)
+{
+	circles.push(camera.position.x+width/2)
+}
+  camera(0, 0, 20 + sin(frameCount * 0.01) * 10, 0, 0, 0, 0, 1, 0);
+ drawSprites();
+}
+
+function keyPressed ()
+{
+  if(keyCode === RIGHT_ARROW)
+  {
+    if(keyIsDown(RIGHT_ARROW))
+    {
+      camera.position.x=camera.position.x+10;
+    }
+  }
+} 
